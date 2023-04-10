e0606b4fb42e2a6b75d3bc484465efc83be5548f
calvinwonpark <calvinwonpark@gmail.com>
2023-04-09 23:31:37 -0700

    suggestion-book-movie

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ from datetime import datetime
 from jinja2 import StrictUndefined
 import requests
 import os
+import json
 
 app = Flask(__name__)
 app.secret_key = "dev"
@@ -22,25 +23,21 @@ def homepage():
     return render_template('homepage.html')
 
 
-
-
 @app.route('/api/validateUser', methods=['GET'])
 def validate_user():
     """Validate User input from login submission."""
     request_email = request.args.get("email")
     request_password = request.args.get("password")
 
-    user = crud.get_user_by_email(request_email) 
+    user = crud.get_user_by_email(request_email)
 
     if not user or user.password != request_password:
-        return jsonify({'success': False, 'message':'The email or password you entered was incorrect.'})
+        return jsonify({'success': False, 'message': 'The email or password you entered was incorrect.'})
     else:
         session['user_id'] = user.user_id
         return jsonify({'success': True,
-                    'email': user.email,
-                    'password': user.password})
-
-
+                        'email': user.email,
+                        'password': user.password})
 
 
 @app.route('/createprofile', methods=['GET'])
@@ -48,7 +45,6 @@ def new_user():
     """Create new user."""
 
     return render_template('newuser.html')
-
 
 
 @app.route('/api/createUser', methods=['POST'])
@@ -59,7 +55,7 @@ def create_user():
     email = request.json.get("email")
     password = request.json.get("password")
     password_2 = request.json.get("rePassword")
-    
+
     user = crud.get_user_by_email(email)
 
     if user:
@@ -72,9 +68,9 @@ def create_user():
             user = crud.create_user(fname, lname, email, password)
             db.session.add(user)
             db.session.commit()
-            user_id = user.user_id 
+            user_id = user.user_id
 
-            webtoons = crud.create_UserList('Webtoons',user_id)
+            webtoons = crud.create_UserList('Webtoons', user_id)
             books = crud.create_UserList('Books', user_id)
             tvshows = crud.create_UserList('TV Shows', user_id)
             movies = crud.create_UserList('Movies', user_id)
@@ -85,7 +81,6 @@ def create_user():
             db.session.commit()
             session['user_id'] = user_id
             return jsonify({'success': True})
-
 
 
 @app.route('/profile')
@@ -99,8 +94,7 @@ def user_profile():
     fname = user.fname
     lists = crud.get_all_user_list_by_user_id(user_id)
 
-    return render_template('profile.html', fname=fname, lists = lists)
-
+    return render_template('profile.html', fname=fname, lists=lists)
 
 
 @app.route('/newForm', methods=['GET'])
@@ -108,7 +102,6 @@ def new_form():
     """Allow's user to see form."""
 
     return render_template('form.html')
-
 
 
 @app.route('/createForm', methods=['POST'])
@@ -124,9 +117,9 @@ def create_form():
     rating = request.json.get("rating")
     thoughts = request.json.get("thoughts")
     recommend_or_not = request.json.get("recommend")
-    #Find the media via API AND media table to see if the media has been
-    #If nothing returns
-    media = crud.create_media(type,name,category,summary)
+    # Find the media via API AND media table to see if the media has been
+    # If nothing returns
+    media = crud.create_media(type, name, category, summary)
     db.session.add(media)
     db.session.commit()
 
@@ -136,35 +129,34 @@ def create_form():
         now = datetime.now()
         current_time = now.strftime("%m-%d-%Y")
         created_at = current_time
-        form = crud.create_form(media, user, rating, thoughts, recommend_or_not, created_at)
+        form = crud.create_form(media, user, rating,
+                                thoughts, recommend_or_not, created_at)
         db.session.add(form)
         db.session.commit()
 
         if type.lower() == 'webtoon':
-            user_list = crud.get_list_by_name_and_user_id('Webtoons',user_id)
-            form_map = crud.create_FormMap(user_list.userlist_id,form.form_id)
+            user_list = crud.get_list_by_name_and_user_id('Webtoons', user_id)
+            form_map = crud.create_FormMap(user_list.userlist_id, form.form_id)
             db.session.add(form_map)
             db.session.commit()
 
         if type.lower() == 'book':
-            user_list = crud.get_list_by_name_and_user_id('Books',user_id)
-            form_map = crud.create_FormMap(user_list.userlist_id,form.form_id)
+            user_list = crud.get_list_by_name_and_user_id('Books', user_id)
+            form_map = crud.create_FormMap(user_list.userlist_id, form.form_id)
             db.session.add(form_map)
             db.session.commit()
         if type.lower() == 'tvshow':
-            user_list = crud.get_list_by_name_and_user_id('TV Shows',user_id)
-            form_map = crud.create_FormMap(user_list.userlist_id,form.form_id)
+            user_list = crud.get_list_by_name_and_user_id('TV Shows', user_id)
+            form_map = crud.create_FormMap(user_list.userlist_id, form.form_id)
             db.session.add(form_map)
             db.session.commit()
         if type.lower() == 'movie':
-            user_list = crud.get_list_by_name_and_user_id('Movies',user_id)
-            form_map = crud.create_FormMap(user_list.userlist_id,form.form_id)
+            user_list = crud.get_list_by_name_and_user_id('Movies', user_id)
+            form_map = crud.create_FormMap(user_list.userlist_id, form.form_id)
             db.session.add(form_map)
             db.session.commit()
 
-
         return jsonify({'success': True})
-
 
 
 @app.route('/newList', methods=['GET'])
@@ -182,12 +174,13 @@ def new_list():
         full_forms.append(media_and_form)
     return render_template('createList.html', forms=forms, full_forms=full_forms)
 
+
 @app.route('/createList', methods=['POST'])
 def create_list():
     """Allows user to create a new list."""
     user_id = session["user_id"]
     name = request.json.get("name")
-    
+
     if (name == None):
         return jsonify({'success': False, 'message': "Please fill out all boxes!"})
     else:
@@ -197,6 +190,7 @@ def create_list():
         session['list_id'] = list.userlist_id
 
         return jsonify({'success': True, 'message': "List Created!"})
+
 
 @app.route('/addFormToList', methods=['POST'])
 def add_form_to_list():
@@ -209,12 +203,12 @@ def add_form_to_list():
     list = crud.get_list_by_id(current_list_id)
     # Get form_id from the API request
     form_id = request.json.get("selectedFormId")
-    #Create a form map for the selected form and the created list
+    # Create a form map for the selected form and the created list
     form_map = crud.create_FormMap(list.userlist_id, form_id)
     db.session.add(form_map)
     db.session.commit()
 
-    return  jsonify({'success': True})
+    return jsonify({'success': True})
 
 
 @app.route('/viewLists/<list_id>', methods=['GET'])
@@ -236,35 +230,133 @@ def view_users_lists(list_id):
         media_and_form['form'] = form
         media_and_form['media'] = media
         full_forms.append(media_and_form)
-    
+
     return render_template('viewList.html', name=current_list.name, full_forms=full_forms)
+
 
 @app.route('/api/suggestions')
 def get_suggestions():
     query = request.args.get('query')
-    # Make a request to the TMDb API to search for movies and TV shows
-    tmdb_api_key = '34e6115227f3afeb13733d2f8978dae9'
-    url = f'https://api.themoviedb.org/3/search/multi?api_key={tmdb_api_key}&query={query}'
-    response = requests.get(url)
-    data = response.json()
-
+    media_type = request.args.get('type')
+    print('TYPE')
+    print(media_type)
     results = []
-    for result in data['results']:
-        if result['media_type'] == 'movie':
-            category = 'Movie'
-        elif result['media_type'] == 'tv':
-            category = 'TV Show'
+
+    if media_type == 'movie':
+        # Make a request to the TMDb API to search for movies and TV shows
+        tmdb_api_key = '34e6115227f3afeb13733d2f8978dae9'
+        url = f'https://api.themoviedb.org/3/search/multi?api_key={tmdb_api_key}&query={query}'
+        response = requests.get(url)
+        data = response.json()
+
+        for result in data['results']:
+            if result['media_type'] == 'movie':
+                summary = result['overview']
+                title = ""
+                if 'title' in result:
+                    title = result['title']
+                elif 'name' in result:
+                    title = result['name']
+                movie_id = result['id']
+
+                new_url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={tmdb_api_key}"
+                new_response = requests.get(new_url)
+                genres = new_response.json().get('genres', [])
+                genre = ""
+                if genres:
+                    genre = genres[0]['name']
+
+                results.append(
+                    {'category': genre, 'summary': summary, 'title': title})
+            else:
+                continue
+
+        # Return the list of suggestions as a JSON response
+        return jsonify(results)
+    elif media_type == 'tvshow':
+        tmdb_api_key = '34e6115227f3afeb13733d2f8978dae9'
+        url = f'https://api.themoviedb.org/3/search/multi?api_key={tmdb_api_key}&query={query}'
+        response = requests.get(url)
+        data = response.json()
+
+        for result in data['results']:
+            if result['media_type'] == 'tv':
+                category = 'TV Show'
+                summary = result['overview']
+                title = ""
+                if 'title' in result:
+                    title = result['title']
+                elif 'name' in result:
+                    title = result['name']
+                movie_id = result['id']
+
+                new_url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={tmdb_api_key}"
+                new_response = requests.get(new_url)
+                genres = new_response.json().get('genres', [])
+                genre = ""
+                if genres:
+                    genre = genres[0]['name']
+
+                results.append(
+                    {'category': genre, 'summary': summary, 'title': title})
+            else:
+                continue
+        return jsonify(results)
+    elif media_type == 'book':
+        url = f"http://openlibrary.org/search.json?q={query}"
+        response = requests.get(url)
+
+        # Check if the request was successful (i.e. status code 200)
+        if response.status_code == 200:
+            # Parse the response JSON and extract the list of search results
+            data = response.json()
+            search_results = data["docs"]
+
+            # Print the title and author of each search result
+            for result in search_results:
+                # print('RESULT')
+                # print(result)
+                book_id = result["seed"][0].replace("/books/", "")
+
+                # print('BOOKID')
+                # print(book_id)
+
+                # Send a GET request to the Open Library API for the book summary
+                summary_url = f"http://openlibrary.org/api/books?bibkeys=OLID:{book_id}&format=json&jscmd=details"
+                # print(summary_url)
+                summary_response = requests.get(summary_url)
+                summary_data = summary_response.json()
+                # print('SUMMARY DATA!!!!')
+                # print(summary_data)
+                # print(summary_data[f"OLID:{book_id}"]["details"])
+                # summary = summary_data[f"OLID:{book_id}"]["details"]["description"]
+                # print('rESULT!!!')
+                # print(summary)
+                title = result["title"]
+                author = result.get("author_name", ["Unknown"])[0]
+                # print('SUMMARY!')
+                # print(summary)
+                # print(f"{title} by {author}")
+                category = ''
+                results.append(
+                    {'category': category, 'summary': "", 'title': f"{title} by {author}"})
         else:
-            continue
-        summary = result['overview']
-        title = ""
-        if 'title' in result:
-            title = result['title']
-        elif 'name' in result:
-            title = result['name']
-        results.append(
-            {'category': category, 'summary': summary, 'title': title})
-    # Return the list of suggestions as a JSON response
+            print(f"Error: {response.status_code} - {response.reason}")
+
+        return jsonify(results)
+
+    elif media_type == 'webtoon':
+        # with open('updated_example.json', encoding="utf-8") as f:
+        #     data = json.load(f)
+
+        # print(data)
+
+        # book_title = data[0]["title"]
+        # results.append(
+        #     {'category': category, 'summary': '', 'title': book_title})
+
+        return jsonify(results)
+
     return jsonify(results)
 
 
--- static/JS/form.js
+++ static/JS/form.js
@@ -6,9 +6,8 @@
 
 function showSuggestions() {
     var searchTerm = document.getElementById("searchBox").value;
-    console.log('searchTerm');
-    console.log(searchTerm);
-    fetch(`/api/suggestions?query=${searchTerm}`)
+    var searchType = document.getElementById("type-select").value;
+    fetch(`/api/suggestions?type=${searchType}&query=${searchTerm}`)
         .then(response => response.json())
         .then(suggestions => displaySuggestions(suggestions))
         .catch(error => console.error(error));
@@ -18,31 +17,46 @@ function displaySuggestions(suggestions) {
     var modal = document.getElementById("myModal");
     var suggestionList = document.getElementById("suggestionList");
     suggestionList.innerHTML = "";
-    for (var i = 0; i < suggestions.length; i++) {
-        var suggestion = suggestions[i];
-        var suggestionDiv = document.createElement("div");
-        suggestionDiv.addEventListener("click", function (event) {
-            var title = event.currentTarget.querySelector(".suggestion-title").innerHTML;
-            var summary = event.currentTarget.querySelector(".suggestion-summary").innerHTML;
-            document.getElementById("summary-box").value = summary;
+    if (suggestions.length > 0) {
+        for (var i = 0; i < suggestions.length; i++) {
+            var suggestion = suggestions[i];
+            var suggestionDiv = document.createElement("div");
+            suggestionDiv.addEventListener("click", function (event) {
+                var title = event.currentTarget.querySelector(".suggestion-title").innerHTML;
+                var summary = event.currentTarget.querySelector(".suggestion-summary").innerHTML;
+                var category = event.currentTarget.querySelector(".suggestion-category").innerHTML;
+
+                document.getElementById("summary-box").value = summary;
+                document.getElementById("category-box").value = category;
+                document.getElementById("searchBox").value = title;
+                modal.style.display = "none";
+            });
+            suggestionDiv.className = "suggestion";
+            var suggestionTitle = document.createElement("div");
+            suggestionTitle.className = "suggestion-title";
+            suggestionTitle.innerHTML = suggestion.title;
+            suggestionDiv.appendChild(suggestionTitle);
+            var suggestionCategory = document.createElement("div");
+            suggestionCategory.className = "suggestion-category";
+            suggestionCategory.innerHTML = suggestion.category;
+            suggestionDiv.appendChild(suggestionCategory);
+            suggestionList.appendChild(suggestionDiv);
+            var suggestionSummary = document.createElement("div");
+            suggestionSummary.className = "suggestion-summary";
+            suggestionSummary.innerHTML = suggestion.summary;
+            suggestionDiv.appendChild(suggestionSummary);
+            suggestionList.appendChild(suggestionDiv);
+        }
+        modal.style.display = "block";
+        var closeBtn = document.getElementsByClassName("close")[0];
+        closeBtn.onclick = function () {
             modal.style.display = "none";
-        });
-        suggestionDiv.className = "suggestion";
-        var suggestionTitle = document.createElement("div");
-        suggestionTitle.className = "suggestion-title";
-        suggestionTitle.innerHTML = suggestion.title;
-        suggestionDiv.appendChild(suggestionTitle);
-        var suggestionSummary = document.createElement("div");
-        suggestionSummary.className = "suggestion-summary";
-        suggestionSummary.innerHTML = suggestion.summary;
-        suggestionDiv.appendChild(suggestionSummary);
-        suggestionList.appendChild(suggestionDiv);
+        }
+    } else {
+        alert('No suggestion found, please continue filling out the form');
+
     }
-    modal.style.display = "block";
-    var closeBtn = document.getElementsByClassName("close")[0];
-    closeBtn.onclick = function () {
-        modal.style.display = "none";
-    }
+
 }
 
 document.getElementById('new_form').addEventListener('submit', (evt) => {
